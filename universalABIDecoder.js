b114e6b518b959aeff3a03389d36cfc16a215307
nicolas.hovette <nicolas.hovette@sii.fr>
2024-09-03 10:26:25 +0200

    AbiDecoder working for V2 Swap exact In

--- universalABIDecoder.js
+++ universalABIDecoder.js
@@ -1,23 +1,45 @@
-const fs = require("fs");
 const {Interface, AbiCoder} = require("ethers")
-const {web3} = require("./web3.js")
-const {universalAbi} = require("./UNIVERSAL_ABI.js")
+const {universalAbi} = require("./UNIVERSAL_ABI.js");
+
+
+const swapCodes = {
+    "00": "V3_SWAP_EXACT_IN",
+    "01": "V3_SWAP_EXACT_OUT",
+    "08": "V2_SWAP_EXACT_IN",
+    "09": "V2_SWAP_EXACT_OUT",
+    "0a": "PERMIT2_PERMIT",
+    "0b": "WRAP_ETH"
+};
 
 
 let universalInterface = new Interface(universalAbi);
 
-function decodedExecute(transactionData){
-    const parsedTx = universalInterface.parseTransaction({data: transactionData})
-    if (null != parsedTx) return
-    console.log(parsedTx)
+function decodedExecute(transactionData,tx){
+    const parsedTx = universalInterface.parseTransaction({data: transactionData.input})
+    
+    if (!parsedTx) return
+    const command = parsedTx.args[0].substring(2).match(/.{1,2}/g)
+    let foundFunction;
+    let inputForFunction;
+    command.forEach(
+        commandCode => {
+            const currentIndex = Object.keys(swapCodes).indexOf(commandCode)
+            if (currentIndex !== -1) {
+                foundFunction = commandCode;
+                inputForFunction = parsedTx.args[1][command.indexOf(commandCode)];
+            }
+        }
+    )
+
+    if (swapCodes[foundFunction] != "V2_SWAP_EXACT_IN") return
+    const abiCoder = new AbiCoder();
+    let decoded = abiCoder.decode(["address", "uint256", "uint256", "address[]", "bool"], inputForFunction);
+    return {
+        function: swapCodes[foundFunction],
+        recipient: decoded[0],
+        amountIn: decoded[1].toString(),
+        amountOutMin: decoded[2].toString(),
+        path: decoded[3],
+        payerIsUser: decoded[4]
+    }
 }
-
-
-web3.eth.subscribe('pendingTransactions').then(subscription => {
-    subscription.on('data', async (tx) => {
-        const transaction = await web3.eth.getTransaction(tx);
-        // console.log(transaction)
-        decodedExecute(transaction.input)
-    })
-})
-
